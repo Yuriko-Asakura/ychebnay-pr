f9be3272ce7632c0ac1684adee2ecbdcb692d7ca
Yriko-Asakura <vasiliev321@mail.ru>
2023-03-30 17:43:12 +0300

    fr

---
+++ ychebpr1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ychebpr1.yc1DataSetTableAdapters;
+namespace ychebpr1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CharactesTableAdapter charactes = new CharactesTableAdapter();
+        NameeTableAdapter name = new NameeTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ycheb.ItemsSource = charactes.GetData();
+            Combo.ItemsSource = name.GetData();
+            Combo.DisplayMemberPath = "namee";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Charactes charactes = new Charactes();
+            charactes.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            classs classs = new classs();
+            classs.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            element element = new element();
+            element.Show();
+        }
+
+        private void Combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            object sel = (Combo.SelectedItem as DataRowView).Row[0];
+            MessageBox.Show(sel.ToString());
+        }
+    }
+}
---
+++ ychebpr1/classs.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ychebpr1.yc1DataSetTableAdapters;
+
+namespace ychebpr1
+{
+    /// <summary>
+    /// Логика взаимодействия для classs.xaml
+    /// </summary>
+    public partial class classs : Window
+    {
+
+        classTableAdapter clas = new classTableAdapter();
+        public classs()
+        {
+            InitializeComponent();
+            ycheb.ItemsSource = clas.GetData();
+        }
+    }
+}
